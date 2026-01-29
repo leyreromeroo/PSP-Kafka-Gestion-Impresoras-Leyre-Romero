f04fc69914e2f6a009ea7d5f45df12f9b3bb137e
Leyre Romero Velasco <leyre.romero@gmail.com>
2026-01-29 11:11:12 +0100

    feat: estructura inicial del proyecto maven para el gestor de impresión

---
+++ GestionColaImpresion/src/main/java/com/cuatrovientos/kafka/GestionColaImpresion/App.java
@@ -0,0 +1,10 @@
+package com.cuatrovientos.kafka.GestionColaImpresion;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
